b16728a6ac24e70d6d4baf07c620cf2e52008d96
Robin Doer <robin@robind.de>
2013-10-19 17:31:24 +0200

    KexdhReply: The kex_algorithm updates p and g

--- lib/nexop/message/kexdh_reply.rb
+++ lib/nexop/message/kexdh_reply.rb
@@ -5,4 +5,20 @@ class Nexop::Message::KexdhReply < Nexop::Message::Base
   add_field(:k_s, type: :string)
   add_field(:f, type: :string)
   add_field(:sig_h, type: :string)
+
+  attr_accessor :kex_algorithm
+  attr_reader :p
+  attr_reader :g
+
+  def kex_algorithm=(algorithm)
+    p, g = case algorithm
+    when "diffie-hellman-group1-sha1" then [ Nexop::Prime::MODP_GROUP1, 2 ]
+    when "diffie-hellman-group14-sha1" then [ Nexop::Prime::MODP_GROUP14, 2 ]
+    else raise ArgumentError, "invalid algorithm: #{algorithm}"
+    end
+
+    @kex_algorithm = algorithm
+    @p = Nexop::Prime.to_i(p)
+    @g = g
+  end
 end
--- spec/nexop/message/kexdh_reply_spec.rb
+++ spec/nexop/message/kexdh_reply_spec.rb
@@ -18,4 +18,56 @@ describe Nexop::Message::KexdhReply do
   it "has a sig_h field" do
     msg.sig_h.should be_nil
   end
+
+  context "kex_algorithm" do
+    it "readable" do
+      msg.kex_algorithm.should be_nil
+    end
+
+    it "is writable with diffie-hellman-group1-sha1" do
+      msg.kex_algorithm = "diffie-hellman-group1-sha1"
+      msg.kex_algorithm.should == "diffie-hellman-group1-sha1"
+    end
+
+    it "is writeable with diffie-hellman-group14-sha1" do
+      msg.kex_algorithm = "diffie-hellman-group14-sha1"
+      msg.kex_algorithm.should == "diffie-hellman-group14-sha1"
+    end
+
+    it "cannot be updates with an invalid algorithm" do
+      expect{ msg.kex_algorithm = "xxx" }.to raise_error(ArgumentError)
+    end
+  end
+
+  context "p" do
+    it "is nil by default" do
+      msg.p.should be_nil
+    end
+
+    it "is updated, when the kex_algorithm is set to diffie-hellman-group1-sha1" do
+      msg.kex_algorithm = "diffie-hellman-group1-sha1"
+      msg.p.should == Nexop::Prime.to_i(Nexop::Prime::MODP_GROUP1)
+    end
+
+    it "is updated, when the kex_algorithm is set to diffie-hellman-group14-sha1" do
+      msg.kex_algorithm = "diffie-hellman-group14-sha1"
+      msg.p.should == Nexop::Prime.to_i(Nexop::Prime::MODP_GROUP14)
+    end
+  end
+
+  context "g" do
+    it "is nil by default" do
+      msg.g.should be_nil
+    end
+
+    it "is updated, when the kex_algorithm is set to diffie-hellman-group1-sha1" do
+      msg.kex_algorithm = "diffie-hellman-group1-sha1"
+      msg.g.should == 2
+    end
+
+    it "is updated, when the kex_algorithm is set to diffie-hellman-group14-sha1" do
+      msg.kex_algorithm = "diffie-hellman-group14-sha1"
+      msg.g.should == 2
+    end
+  end
 end
