6cb0a0b91b13619959ff604a57cf51b0c3cf300c
Robin Doer <robin@robind.de>
2013-10-26 17:30:11 +0200

    Keystore: Store encryption- and MAC algorithm in the keystore

--- lib/nexop/keystore.rb
+++ lib/nexop/keystore.rb
@@ -1,4 +1,44 @@
 module Nexop
   class Keystore
+    def initialize
+      @encryption_algorithm = Array.new(2, EncryptionAlgorithm::NONE)
+      @mac_algorithm = Array.new(2, MacAlgorithm::NONE)
+    end
+
+    ##
+    # The current encryption algorithm for the given `direction`.
+    #
+    # @param direction [:c2s, :s2c] Specifies the direction of the
+    #        communication. Can be either _client to server_ (`:c2s`) or
+    #        _server to client_ (:s2c).
+    # @return [String] The current encryption algorithm which is used for the
+    #         given `direction`.
+    # @see EncryptionAlgorithm
+    def encryption_algorithm(direction)
+      @encryption_algorithm[dir2idx(direction)]
+    end
+
+    ##
+    # The current MAC algorithm for the given `direction`.
+    #
+    # @param direction [:c2s, :s2c] Specifies the direction of the
+    #        communication. Can be either _client to server_ (`:c2s`) or
+    #        _server to client_ (:s2c).
+    # @return [String] The current MAC algorithm which is used for the given
+    #         `direction`.
+    # @see MacAlgorithm
+    def mac_algorithm(direction)
+      @mac_algorithm[dir2idx(direction)]
+    end
+
+    private
+
+    def dir2idx(direction)
+      case direction
+      when :c2s then 0
+      when :s2c then 1
+      else raise ArgumentError, "invalid direction: #{direction}"
+      end
+    end
   end
 end
--- spec/nexop/keystore_spec.rb
+++ spec/nexop/keystore_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
 
 describe Nexop::Keystore do
+  let(:keystore) { Nexop::Keystore.new }
+
+  context "encryption_algorithm" do
+    [ :c2s, :s2c ].each do |direction|
+      it "is NONE by default for #{direction}" do
+        keystore.encryption_algorithm(direction).should == Nexop::EncryptionAlgorithm::NONE
+      end
+    end
+
+    it "only accepts :c2s and :s2c" do
+      expect{ keystore.encryption_algorithm(:xxx) }.to raise_error(ArgumentError)
+    end
+  end
+
+  context "mac_algorithm" do
+    [ :c2s, :s2c ].each do |direction|
+      it "is NONE by default for #{direction}" do
+        keystore.mac_algorithm(direction).should == Nexop::MacAlgorithm::NONE
+      end
+    end
+
+    it "only accepts :c2s and :s2c" do
+      expect{ keystore.mac_algorithm(:xxx) }.to raise_error(ArgumentError)
+    end
+  end
 end
