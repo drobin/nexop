0076ea8c27c6b384f3fb65c15caccbc2b780093b
Robin Doer <robin@robind.de>
2013-10-28 19:16:21 +0100

    EncryptionAlgorithms is now a class

--- lib/nexop/encryption_algorithm.rb
+++ lib/nexop/encryption_algorithm.rb
@@ -1,7 +1,12 @@
 module Nexop
   ##
   # Supported encryption algorithms.
-  module EncryptionAlgorithm
+  class EncryptionAlgorithm
+    ##
+    # Returns the name of the algorithm.
+    # @return [String]
+    attr_reader :name
+
     ##
     # three-key 3DES in CBC mode
     DES = "3des-cbc"
@@ -18,6 +23,26 @@ module Nexop
     #         returned, `false`otherwise.
     def self.supported?(algorithm)
       [ DES, NONE ].include?(algorithm)
+    end
+
+    ##
+    # Creates a {EncryptionAlgorithm}-instance for the given `algorithm`.
+    #
+    # @param algorithm [String] The name of the algorithm. It should be one
+    #        of the algorithms defined in the class.
+    # @return [EncryptionAlgorithm] the related
+    #         {EncryptionAlgorithm}-instance
+    def self.from_s(algorithm)
+      if EncryptionAlgorithm.supported?(algorithm)
+        @@instances ||= {}
+        @@instances[algorithm] ||= EncryptionAlgorithm.new(algorithm)
+      end
+    end
+
+    private
+
+    def initialize(algorithm)
+      @name = algorithm
     end
   end
 end
--- spec/nexop/encryption_algorithm_spec.rb
+++ spec/nexop/encryption_algorithm_spec.rb
@@ -1,15 +1,45 @@
 require 'spec_helper'
 
 describe Nexop::EncryptionAlgorithm do
-  it "DES is a valid algorithm" do
-    Nexop::EncryptionAlgorithm.supported?(Nexop::EncryptionAlgorithm::DES).should be_true
+  ALL_ALGORITHMS = [
+    Nexop::EncryptionAlgorithm::DES,
+    Nexop::EncryptionAlgorithm::NONE
+  ]
+
+  context "supported?" do
+    ALL_ALGORITHMS.each do |algorithm|
+      it "#{algorithm} is a valid algorithm" do
+        Nexop::EncryptionAlgorithm.supported?(algorithm).should be_true
+      end
+
+      it "xxx is an invalid algorithm" do
+        Nexop::EncryptionAlgorithm.supported?("xxx").should be_false
+      end
+    end
   end
 
-  it "NONE is a valid algorithm" do
-    Nexop::EncryptionAlgorithm.supported?(Nexop::EncryptionAlgorithm::NONE).should be_true
+  context "from_s" do
+    ALL_ALGORITHMS.each do |algorithm|
+      it "creates an instance from #{algorithm}" do
+        Nexop::EncryptionAlgorithm.from_s(algorithm).should be_a_kind_of(Nexop::EncryptionAlgorithm)
+      end
+
+      it "returns a singleton instance for #{algorithm}" do
+        instance = Nexop::EncryptionAlgorithm.from_s(algorithm)
+        Nexop::EncryptionAlgorithm.from_s(algorithm).should equal(instance)
+      end
+    end
+
+    it "cannot create an instance from an invalid algorithm" do
+      Nexop::EncryptionAlgorithm.from_s("xxx").should be_nil
+    end
   end
 
-  it "xxx is an invalid algorithm" do
-    Nexop::EncryptionAlgorithm.supported?("xxx").should be_false
+  context "name" do
+    ALL_ALGORITHMS.each do |algorithm|
+      it "returns the name of #{algorithm}" do
+        Nexop::EncryptionAlgorithm.from_s(algorithm).name.should == algorithm
+      end
+    end
   end
 end
