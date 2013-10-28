4e38ad8abdbafd6fb6b28a1e7993e6e95052bf01
Robin Doer <robin@robind.de>
2013-10-28 19:26:33 +0100

    MacAlgorithm is now a class

--- lib/nexop/mac_algorithm.rb
+++ lib/nexop/mac_algorithm.rb
@@ -1,5 +1,10 @@
 module Nexop
-  module MacAlgorithm
+  class MacAlgorithm
+    ##
+    # Returns the name of the algorithm.
+    # @return [String]
+    attr_reader :name
+
     ##
     # HMAC-SHA1 (digest length = key length = 20)
     SHA1 = "hmac-sha1"
@@ -16,6 +21,25 @@ module Nexop
     #         returned, `false`otherwise.
     def self.supported?(algorithm)
       [ SHA1, NONE ].include?(algorithm)
+    end
+
+    ##
+    # Creates a {MacAlgorithm}-instance for the given `algorithm`.
+    #
+    # @param algorithm [String] The name of the algorithm. It should be one
+    #        of the algorithms defined in the class.
+    # @return [MacAlgorithm] the related {MacAlgorithm}-instance
+    def self.from_s(algorithm)
+      if MacAlgorithm.supported?(algorithm)
+        @@instances ||= {}
+        @@instances[algorithm] ||= MacAlgorithm.new(algorithm)
+      end
+    end
+
+    private
+
+    def initialize(algorithm)
+      @name = algorithm
     end
   end
 end
--- spec/nexop/mac_algorithm_spec.rb
+++ spec/nexop/mac_algorithm_spec.rb
@@ -1,15 +1,45 @@
 require 'spec_helper'
 
 describe Nexop::MacAlgorithm do
-  it "SHA1 is a supported algorithm" do
-    Nexop::MacAlgorithm::supported?(Nexop::MacAlgorithm::SHA1).should be_true
+  ALL_ALGORITHMS = [
+    Nexop::MacAlgorithm::SHA1,
+    Nexop::MacAlgorithm::NONE
+  ]
+
+  context "supported?" do
+    ALL_ALGORITHMS.each do |algorithm|
+      it "#{algorithm} is a supported algorithm" do
+        Nexop::MacAlgorithm::supported?(algorithm).should be_true
+      end
+    end
+
+    it "xxx is not a supported algorithm" do
+      Nexop::MacAlgorithm::supported?("xxx").should be_false
+    end
   end
 
-  it "NONE is a supported algorithm" do
-    Nexop::MacAlgorithm::supported?(Nexop::MacAlgorithm::NONE).should be_true
+  context "from_s" do
+    ALL_ALGORITHMS.each do |algorithm|
+      it "creates an instance from #{algorithm}" do
+        Nexop::MacAlgorithm.from_s(algorithm).should be_a_kind_of(Nexop::MacAlgorithm)
+      end
+
+      it "returns a singleton instance for #{algorithm}" do
+        instance = Nexop::MacAlgorithm.from_s(algorithm)
+        Nexop::MacAlgorithm.from_s(algorithm).should equal(instance)
+      end
+    end
+
+    it "cannot create an instance from an invalid algorithm" do
+      Nexop::MacAlgorithm.from_s("xxx").should be_nil
+    end
   end
 
-  it "xxx is not a supported algorithm" do
-    Nexop::MacAlgorithm::supported?("xxx").should be_false
+  context "name" do
+    ALL_ALGORITHMS.each do |algorithm|
+      it "returns the name of #{algorithm}" do
+        Nexop::MacAlgorithm.from_s(algorithm).name.should == algorithm
+      end
+    end
   end
 end
