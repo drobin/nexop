c9768c18d2606b82109cdc71d8211fbb30408a57
Robin Doer <robin@robind.de>
2013-11-01 13:40:25 +0100

    Service-handler: register service-instances

--- lib/nexop/session/service.rb
+++ lib/nexop/session/service.rb
@@ -6,6 +6,39 @@ module Nexop
     # @see http://tools.ietf.org/html/rfc4253#section-10
     class Service < Handler::Base
       ##
+      # Services registered at the service-handler.
+      #
+      # Services registered here are accepted during the SSH protocol
+      # handshake and can be started on the top on the session.
+      #
+      # @return [Array] Array containing all activated services of type
+      #         {ServiceBase}.
+      attr_reader :services
+
+      def initialize(send_method)
+        super(send_method)
+
+        @services = []
+      end
+
+      ##
+      # Registers a service on the service-handler.
+      #
+      # A registered service can be requested from the client and then be
+      # started on the top of the session.
+      #
+      # @param service [ServiceBase] The service-instance to register
+      # @raise [ArgumentError] if the `service`-class is not derivated from
+      #        {ServiceBase}.
+      def add_service(service)
+        if service.is_a?(Nexop::ServiceBase)
+          @services << service
+        else
+          raise ArgumentError, "invalid service-class: #{service.class.name}"
+        end
+      end
+
+      ##
       # Session-tick implementation for the service-request.
       #
       # The method should be called by the session as long as `true` is
--- spec/nexop/session/service_spec.rb
+++ spec/nexop/session/service_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe Nexop::Handler::Service do
+  let(:receiver) { double(:m => nil)}
+  let(:handler) { Nexop::Handler::Service.new(receiver.method(:m)) }
+
+  context "services" do
+    it "is empty by default" do
+      handler.services.should be_empty
+    end
+  end
+
+  context "add_service" do
+    it "registers a new service" do
+      service = Nexop::ServiceBase.new("foo")
+      handler.add_service(service)
+      handler.services.should == [ service ]
+    end
+
+    it "cannot add an invalid service" do
+      expect{ handler.add_service("xxx") }.to raise_error(ArgumentError)
+    end
+  end
 end
