a8128568bd71ed24b0c126ad4f0ac6b1f783d856
Robin Doer <robin@robind.de>
2013-11-02 10:06:09 +0100

    Adaption of the Handler::Base interface

--- lib/nexop/session/handler.rb
+++ lib/nexop/session/handler.rb
@@ -10,6 +10,65 @@ module Nexop
       #        {Message::Base} back to the client.
       def initialize(send_method)
         @send_method = send_method
+        @finished = false
+      end
+
+      ##
+      # Tests whether the handler has finished its operation.
+      #
+      # @return [Boolean] If `true` is returned, then the handler has
+      #         finished its operation and no more invocation of {#tick} are
+      #         required.
+      # @see #finished=
+      def finished?
+        @finished ||= false
+      end
+
+      ##
+      # Performs a single step of the handler.
+      #
+      # The handler should consume the `payload` (if present) and produce
+      # some output. Depending on the service and the state of the service
+      # the `payload` can be empty!
+      #
+      # The output can be:
+      #
+      # - {Message::Base}: A single message which should be send back to the
+      #   client.
+      # - `Array` of {Message::Base}: The `tick` operation results into more
+      #   than one packet, which are all serialized and send back to the
+      #   client.
+      # - `false`: This is a `NOOP`. The handler should still be active but
+      #   currently produces no output.
+      # - If a {SessionError} is raised, then the handler results into an
+      #   abnormal situation and the session will be destroyed.
+      #
+      # If {#finished?} succeeds, then the handler has finished its
+      # operation. Any message returned by the `tick`-implementation is the
+      # last message send to the client.
+      #
+      # @param payload [String] The payload (raw message data) the handler
+      #        should consume.
+      # @return [Message::Base, Array] A {Message::Base message} or an
+      #         array of {Message::Base messages}, which should be send to
+      #         the client.
+      # @return [false] The method-invocation is interpreted as a `NOOP`.
+      # @raise [SessionError] The handler results into an abnormal situation
+      #        and the session will be destroyed.
+      def tick(payload)
+        raise NotImplementedError, "must be implemented"
+      end
+
+      ##
+      # Handler finalization.
+      #
+      # The method is invoked after the last {#tick}-invocation, when
+      # {#finished?} returns true and the remaining messages where send to
+      # the client.
+      #
+      # @raise [SessionError] The handler results into an abnormal situation
+      #        and the session will be destroyed.
+      def finalize
       end
 
       ##
@@ -21,6 +80,14 @@ module Nexop
       def send_message(message)
         @send_method.call(message)
         self
+      end
+
+      protected
+
+      ##
+      # Updates the {#finished?}-flag.
+      def make_finish
+        @finished = true
       end
     end
   end
--- spec/nexop/session/handler_spec.rb
+++ spec/nexop/session/handler_spec.rb
@@ -2,13 +2,36 @@ require 'spec_helper'
 
 describe Nexop::Handler::Base do
   let(:arg) { "arg" }
-  let(:obj) { double }
+  let(:obj) { double(:a_method => nil) }
   let(:handler) { Nexop::Handler::Base.new(obj.method(:a_method)) }
 
   context "send_message" do
     it "acts as a proxy" do
       obj.should_receive(:a_method).with(arg)
       handler.send_message(arg)
+    end
+  end
+
+  context "finished" do
+    it "is false by default" do
+      handler.should_not be_finished
+    end
+
+    it "can be changed" do
+      handler.send(:make_finish)
+      handler.should be_finished
+    end
+  end
+
+  context "tick" do
+    it "must be implemented" do
+      expect{ handler.tick(nil) }.to raise_error(NotImplementedError)
+    end
+  end
+
+  context "finalize" do
+    it "makes nothing" do
+      handler.finalize
     end
   end
 end
